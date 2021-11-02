5fe816113d345b41711baeb579994226ce5180df
SauravMaheshkar <saurav@wandb.com>
2021-11-02 07:20:15 -0700

    Update Pooling Script

--- enformer/pooling.py
+++ enformer/pooling.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 
 __all__ = ["pooling_module"]
 
+from .layers.pooling_layers import SoftmaxPooling1D
+
 
 def pooling_module(kind, pool_size):
     """Pooling module wrapper."""
@@ -11,39 +13,3 @@ def pooling_module(kind, pool_size):
         return tf.keras.layers.MaxPool1D(pool_size=pool_size, padding="same")
     else:
         raise ValueError(f"Invalid pooling kind: {kind}.")
-
-
-class SoftmaxPooling1D(tf.Module):
-    """Pooling operation with optional weights."""
-
-    def __init__(
-        self,
-        pool_size: int = 2,
-        per_channel: bool = False,
-        w_init_scale: float = 0.0,
-        name: str = "softmax_pooling",
-        **kwargs
-    ):
-        super(SoftmaxPooling1D, self).__init__(name=name, **kwargs)
-        self._pool_size = pool_size
-        self._per_channel = per_channel
-        self._w_init_scale = w_init_scale
-        self._logit_linear = None
-
-    def _initialize(self, num_features):
-        self._logit_linear = tf.keras.layers.Dense(
-            units=num_features if self._per_channel else 1,
-            use_bias=False,  # Softmax is agnostic to shifts.
-            kernel_initializer=tf.keras.initializers.Identity(gain=self._w_init_scale),
-        )
-
-    @tf.Module.with_name_scope
-    def __call__(self, inputs):
-        _, length, num_features = inputs.shape
-        self._initialize(num_features)
-        inputs = tf.reshape(
-            inputs, (-1, length // self._pool_size, self._pool_size, num_features)
-        )
-        return tf.reduce_sum(
-            inputs * tf.nn.softmax(self._logit_linear(inputs), axis=-2), axis=-2
-        )
